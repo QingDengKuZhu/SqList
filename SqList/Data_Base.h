61fb195badd2016877e64d80d6d7ac386a96167e
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2013-12-06 18:05:17 +0800

    修改拼写错误

--- SqList/Data_Base.h
+++ SqList/Data_Base.h
@@ -26,16 +26,16 @@ BOOL ListEmpty(PSQLIST pL);
 size_t ListLength(PSQLIST pL);
 
 //获取顺序线性表中第pos个元素.若操作成功,函数返回OK,pe保存满足条件的结点的地址;若操作失败,函数返回FAILE,pe为NULL.
-SATUES GetList(PSQLIST pL, size_t pos, int *pe);
+STATUS GetList(PSQLIST pL, size_t pos, int *pe);
 
 //返回顺序线性表中第一个与e相等的元素的位序(从1开始).若成功函数返回相应的位序;否者返回0.
 size_t LocateElem(PSQLIST pL, int e);
 
 //在顺序线性表中第pos个位置之前插入新的数据元素e(1 <= pos <= ListLength(pL)+1).若成功,函数返回OK;否者返回FAILE.
-SATUES InsertList(PSQLIST pL, size_t pos, int e);
+STATUS InsertList(PSQLIST pL, size_t pos, int e);
 
 //删除顺序线性表中第pos个元素.若成功删除,函数返回OK,pe保存被删除元素的地址;否者函数返回FAILE,pe为NULL.
-SATUES DeleteList(PSQLIST pL, size_t pos, int *pe);
+STATUS DeleteList(PSQLIST pL, size_t pos, int *pe);
 
 //遍输出顺序线性表
 void TraveList(PSQLIST pL);
