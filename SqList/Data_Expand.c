6fbf3eabc8b7e2106cdadcf12c68a75c9185d30a
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2013-12-13 15:17:29 +0800

    添加扩展操作
    添加支持顺序线性表的操作

---
+++ SqList/Data_Expand.c
@@ -0,0 +1,49 @@
+﻿#include "Data.h"
+#include "Main_First.h"
+#include "Data_Base.h"
+
+STATUS PriorELem(PSQLIST pL, int v, int *pre_e)
+{
+	size_t pos = LocateElem(pL, v);
+	if (pos!=0 && pos!=1)/*如果存在并且不为第一个结点*/
+	{
+		GetList(pL, pos-1, pre_e);
+		return OK;
+	}
+	else
+	{
+		return FALSE;
+	}
+
+}
+STATUS NextELem(PSQLIST pL, int v, int *next_e)
+{
+	size_t pos = LocateElem(pL, v);
+	if (pos!=0 && pos!=ListLength(pL))/*如果存在并且不为第一个结点*/
+	{
+		GetList(pL, pos+1, next_e);
+		return OK;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+
+
+void UnionList(PSQLIST pLa, PSQLIST pLb)
+{
+	size_t i;
+	size_t pos = ListLength(pLb);
+	int v;
+
+	for (i=1; i<=pos; ++i)
+	{
+		GetList(pLb, i, &v);
+		if (0 == LocateElem(pLa, v) )/*比较是否在pLa中*/
+		{
+			InsertList(pLa, ListLength(pLa), v);/*若不在,将数据插入表尾*/
+		}
+	}
+}
---
+++ SqList/Data_Expand.h
@@ -0,0 +1,23 @@
+﻿/**********************
+**顺序线性表的扩展操作.
+***********************/
+#ifndef DATA_EXPAND_H
+#define DATA_EXPAND_H
+
+#include "Main_First.h"
+#include "Data.h"
+
+/*
+pL为指向顺序线性表的指针,且pL不为NULL.
+*/
+
+/*返回顺序线性表的前驱元素.若v是顺序线性表的数据元素,且不是第一个,则用*e返回它的前一个元素,函数返回OK;否者函数返回FALSE, *e无定义*/
+STATUS PriorELem(PSQLIST pL, int v, int *pre_e);
+
+/*返回顺序线性表的后驱元素.若v是顺序线性表的数据元素,且不是第最后个,则用*e返回它的后一个元素,函数返回OK;否者函数返回FALSE, *e无定义*/
+STATUS NextELem(PSQLIST pL, int v, int *next_e);
+
+/*将所有在线性表pLb中但是不在pLa中的数据元素插入到pLa后面*/
+void UnionList(PSQLIST pLa, PSQLIST pLb);
+
+#endif
