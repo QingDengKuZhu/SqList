fc26abe50bd827f7a40c8a8448d83c0ec99021a2
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2013-12-13 14:53:57 +0800

    修改注释

--- SqList/Data_Base.h
+++ SqList/Data_Base.h
@@ -22,22 +22,22 @@ void ClearList(PSQLIST pL);
 /*判断顺序线性表是否为空.若为空,函数返回TRUE;否者函数返回FALSE.*/
 BOOL ListEmpty(PSQLIST pL);
 
-//返回顺序线性表的长度
+/*返回顺序线性表的长度*/
 size_t ListLength(PSQLIST pL);
 
-//获取顺序线性表中第pos个元素.若操作成功,函数返回OK,*e保存满足条件的结点的值;若操作失败,函数返回FAILE,*e为垃圾值.
+/*获取顺序线性表中第pos个元素.若操作成功,函数返回OK,*e保存满足条件的结点的值;若操作失败,函数返回FAILE,*e为垃圾值.*/获取顺序线性表中第pos个元素.若操作成功,函数返回OK,*e保存满足条件的结点的值;若操作失败,函数返回FAILE,*e为垃圾值.
 STATUS GetList(PSQLIST pL, size_t pos, int *e);
 
-//返回顺序线性表中第一个与e相等的元素的位序(从1开始).若成功函数返回相应的位序;否者返回0.
+/*返回顺序线性表中第一个与e相等的元素的位序(从1开始).若成功函数返回相应的位序;否者返回0*/.
 size_t LocateElem(PSQLIST pL, int e);
 
-//在顺序线性表中第pos个位置之前插入新的数据元素e(1 <= pos <= ListLength(pL)+1).若成功,函数返回OK;否者返回FAILE.
+/*在顺序线性表中第pos个位置之前插入新的数据元素e(1 <= pos <= ListLength(pL)+1).若成功,函数返回OK;否者返回FAILE.*/
 STATUS InsertList(PSQLIST pL, size_t pos, int e);
 
-//删除顺序线性表中第pos个元素.若成功删除,函数返回OK,*e保存被删除元素的值;否者函数返回FAILE,*e为垃圾值.
+/*删除顺序线性表中第pos个元素.若成功删除,函数返回OK,*e保存被删除元素的值;否者函数返回FAILE,*e为垃圾值.*/
 STATUS DeleteList(PSQLIST pL, size_t pos, int *e);
 
-//遍输出顺序线性表
+/*遍输出顺序线性表*/
 void TraveList(PSQLIST pL);
 
 #endif
